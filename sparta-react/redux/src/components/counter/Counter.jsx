71f243876047429eaa320a385f75f82e022dc20e
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-04 14:15:39 +0900

    disaptch 와 액션함수 액션 크리에이트 사용

--- sparta-react/redux/src/components/counter/Counter.jsx
+++ sparta-react/redux/src/components/counter/Counter.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { decrease, increase } from "../../redux/modules/counter";
 
 function Counter() {
   const data = useSelector((data) => data.counter);
-  console.log(data);
+  const dispatch = useDispatch();
 
-  return <div></div>;
+  return (
+    <StyledCounter>
+      <h1>Counter : {data.number}</h1>
+      <button onClick={() => dispatch(increase())}>1더하기</button>
+      <button onClick={() => dispatch(decrease())}>1빼기</button>
+    </StyledCounter>
+  );
 }
 
 export default Counter;
+
+const StyledCounter = styled.div`
+  border: 1px solid black;
+  width: 300px;
+  height: 300px;
+`;
+
+//  useSelector를 사용했을 때 객체형태를 그대로 사용하려고 하면 Objects are not valid as a React child 이와 같은 에러가 난다.
+// 해결방법 : 객체 형태로 사용하지 말고 객체 안의 값을 뽑아서 사용하자 ex) {data} X , {data.number} O
+// 더하고 빼는 onCLick에 호출형태를 넣으면 계속해서 실행이 되는 모습을 보인다 그렇기에 함수형태로 넣어줘야한다(아제 이해한것 같다)
--- sparta-react/redux/src/redux/modules/counter.js
+++ sparta-react/redux/src/redux/modules/counter.js
@@ -1,9 +1,10 @@
-// 액션객체
-const INCREASEMENT = "counter/INCREASEMENT"
-const DECREASEMENT = "counter/DECREASEMENT"
-
 // 액션함수
+const INCREASEMENT = "INCREASEMENT";
+const DECREASEMENT = "DECREASEMENT";
 
+// 액션 크리에이트
+export const increase = () => ({ type: INCREASEMENT });
+export const decrease = () => ({ type: DECREASEMENT });
 
 
 
@@ -17,18 +18,15 @@ const initalState = {
 const counter = (state = initalState, action) => {
   switch (action.type) {
     case INCREASEMENT:
-      return {
-      ...state,
-        count: state.count + 1,
-      };
+      return {...state, number : state.number + 1 };
     case DECREASEMENT:
-      return {
-      ...state,
-        count: state.count - 1,
-      };
+      return  {...state, number : state.number - 1 };
     default:
       return state;
   }
 }
 
 export default counter;
+
+// 숫자를 더하는 방법을 까먹었는데 불변성을 지키면서 숫자 1을 더하는 방법
+// 왜 위에 방식은 불변성을 지키지 못했는가? => 불변성을 못지킨게 아니고 액션의 타입이 달랐음
