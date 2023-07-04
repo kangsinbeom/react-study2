a1561955f7f00d7e90aeedfe946724eaab5a2680
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-04 13:28:09 +0900

    store 생성

---
+++ sparta-react/redux/src/redux/config/store.js
@@ -0,0 +1,7 @@
+import {combineReducers} from'redux';
+import { createStore } from'redux';
+
+const rootReducer = combineReducers({});
+const store = createStore(rootReducer);
+
+export default store;
