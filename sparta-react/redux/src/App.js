4120068b4037ea2793f7996342f4803f326f6a1e
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-04 14:49:40 +0900

    router start

--- sparta-react/redux/src/App.js
+++ sparta-react/redux/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Counter from './components/counter/Counter';
+import ReactRouterDom from'./components/router/ReactRouterDom';
+
 function App() {
   return (
     <div>
       <Counter />
+      <ReactRouterDom />
     </div>
   );
 }
---
+++ sparta-react/redux/src/components/router/ReactRouterDom.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReactRouterDom() {
+  return <div></div>;
+}
+
+export default ReactRouterDom;
