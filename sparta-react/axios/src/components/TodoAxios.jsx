976c92cc90089fd058a679008cb7a4e3034c2c09
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-07-05 17:34:15 +0900

    env는 안올라가야하지않나?

--- sparta-react/axios/src/components/TodoAxios.jsx
+++ sparta-react/axios/src/components/TodoAxios.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-
+import api from "../axios/api";
 function TodoAxios() {
   const [todos, setTodos] = useState(null);
   const [updateValue, setUpdateValue] = useState({
@@ -14,13 +13,13 @@ function TodoAxios() {
 
   // 조회함수
   const fetchTodos = async () => {
-    const { data } = await axios.get("http://localhost:4000/todos");
+    const { data } = await api.get("/todos");
     setTodos(data);
   };
 
   // 추가함수
   const onSubmitHnadler = async () => {
-    await axios.post("http://localhost:4000/todos", inputValue);
+    await api.post(`/todos`, inputValue);
     fetchTodos();
     // 여기서 setTodos([...todos, inputValue]) 로 했었는데 이게 에러가 뜬다 그래서 걍 fetchTodo() 로 빠꿈
     setInputValue({
@@ -31,13 +30,13 @@ function TodoAxios() {
 
   // 삭제함수
   const onDeleteHandler = async (id) => {
-    await axios.delete(`http://localhost:4000/todos/${id}`);
+    await api.delete(`/todos/${id}`);
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   // 수정함수
   const onUpdateHandler = async () => {
-    await axios.patch(`http://localhost:4000/todos/${updateValue.id}`, {
+    await api.patch(`/todos/${updateValue.id}`, {
       title: updateValue.title,
     });
     fetchTodos();
